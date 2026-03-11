c1b14ed562476453572bed1350b803e052c2ad62
your name <ayushvk26102006@gmail.com>
2026-03-11 09:39:15 +0530

    js in second sem

---
+++ dom.js
@@ -0,0 +1,9 @@
+function show(){
+    let name=document.getElementById("myName").value;
+            // document.getElementById("display").innerHTML=name;
+             alert(name);
+        }
+        //dom on 11th march
+        function changecolor(){
+            document.getElementById('text').style.color="brown";
+        }
